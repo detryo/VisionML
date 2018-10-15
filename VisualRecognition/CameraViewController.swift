4bb9e7e808ee248e4d111d82324b2ac8dfd13229
Cristian Sedano Arenas <cristiansedanoarenas@mbp-de-cristian.home>
2018-10-15 16:27:19 +0100

    Init Camera
    fill func initCamera

--- VisualRecognition/CameraViewController.swift
+++ VisualRecognition/CameraViewController.swift
@@ -3,13 +3,22 @@ import AVFoundation
 import CoreML
 import Vision
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     @IBOutlet var cameraView: UIView!
     @IBOutlet var tempImageView: UIImageView!
     
     @IBOutlet var captureButton: UIButton!
     @IBOutlet var retakeButton: UIButton!
+    
+    // Variables de la Camara
+    var captureSession: AVCaptureSession?
+    var photoOutput: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
     
     override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -18,6 +27,58 @@ class CameraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initCamera()
+        retake()
+        
+    }
+    
+    func initCamera() {
+        self.captureSession = AVCaptureSession()
+        self.captureSession?.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            self.captureSession?.addInput(input)
+            
+            self.photoOutput = AVCapturePhotoOutput()
+            if (self.captureSession?.canAddOutput(self.photoOutput!) != nil) {
+                self.captureSession?.addOutput(self.photoOutput!)
+                
+                self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession!)
+                self.previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+                self.previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                self.cameraView.layer.addSublayer(self.previewLayer!)
+                self.captureSession?.startRunning()
+            }
+            
+        } catch {
+            print("ERROR: \(error)")
+        }
+        self.previewLayer?.frame = self.view.bounds
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+    }
+    
+    func classify(_ image: CGImage, completion: @escaping ([VNClassificationObservation]) -> Void) {
+        
+    }
+    
+    func dismissResults() {
+        
+        
+    }
+    
+    func push(data: [VNClassificationObservation]) {
+        
+    }
+    
+    func getTableController(run: (_ tableController: ResultsTableViewController,
+        _ drawer: PulleyViewController) -> Void) {
+        
     }
     
     @IBAction func takePhoto() {
@@ -25,6 +86,10 @@ class CameraViewController: UIViewController {
     }
     
     @IBAction func retake() {
+        self.tempImageView.isHidden = true
+        self.captureButton.isHidden = false
+        self.retakeButton.isHidden  = true
         
+        dismissResults()
     }
 }
--- VisualRecognition/ResultsTableViewController.swift
+++ VisualRecognition/ResultsTableViewController.swift
@@ -3,6 +3,9 @@ import Vision
 
 class ResultsTableViewController: UITableViewController {
    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
